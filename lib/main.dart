aaac0da66d5cd38794d03f62a0636b762c7ea931
Lutas <lutas@handsup.shop>
2019-11-12 19:00:05 +0800

    test html parser

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app/routes/web.dart';
 
 import 'routes/home.dart';
 
@@ -8,9 +9,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      initialRoute: '/',
+      initialRoute: '/web',
       routes: {
-        '/' : (context) => HomePage()
+        '/' : (context) => HomePage(),
+        '/web' : (context) => WebPage()
       },
       title: 'Flutter Demo',
       theme: ThemeData(
